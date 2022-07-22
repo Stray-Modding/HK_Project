ad117d6aa39aff6739165acec086c3085893485c
Narknon <narknon0@gmail.com>
2022-07-22 14:16:16 -0400

    Create Hk_projectGame.Target.cs

---
+++ Source/Hk_projectGame.Target.cs
@@ -0,0 +1,11 @@
+﻿using UnrealBuildTool;
+
+public class Hk_projectGameTarget : TargetRules {
+	public Hk_projectGameTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"Hk_project",
+		});
+	}
+}
